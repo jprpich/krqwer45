f2ecc28f6e30baf4105b5a6fb00117f758d550c9
Joshua <joshuaprpich@gmail.com>
2018-04-23 18:11:15 -0500

    resolve challenge

--- src/App.js
+++ src/App.js
@@ -11,19 +11,56 @@ class App extends Component {
         { id: 2, name: "Hacer la cama", done: true },
         { id: 3, name: "Leer un rato", done: false }
       ],
-      newTask: ''
+      newTask: '',
+      hasError: false
+
     }
+    this.addTask = this.addTask.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.updateTask = this.updateTask.bind(this)
   }
+
+  addTask(event){
+    if (this.state.newTask === '') {
+      this.setState({
+        hasError: true
+      })
+    } else {
+      this.setState({
+        tasks: this.state.tasks.concat({name: this.state.newTask, done: false}),
+        newTask: ''
+      })    
+    }
+    event.preventDefault();
+  }
+
+  handleChange(event){
+    this.setState({
+      newTask: event.target.value
+    })
+  }
+
+  updateTask(event){
+    const index = this.state.tasks.findIndex(task =>
+      task.name === event.target.innerHTML
+      );
+    this.setState({
+      tasks: this.state.tasks.map((task, i) =>
+        i === index ? {name: task.name, done: !task.done}: task
+      )
+    })
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li key={index} onClick={this.updateTask} className={task.done ? "done" : ""}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.addTask}>
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" onChange={this.handleChange} value={this.state.newTask} className={this.state.hasError ? "error" : ""}/>
           </form>
         </div>
       </div>
